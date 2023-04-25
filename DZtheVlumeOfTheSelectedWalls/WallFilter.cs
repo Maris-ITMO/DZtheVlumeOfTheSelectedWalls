ae0991f2dc5cdd91a3621beefffedd5a581b043b
Марис <122129172+Maris-ITMO@users.noreply.github.com>
2023-04-25 18:07:04 +0300

    Add project files.

---
+++ DZtheVlumeOfTheSelectedWalls/WallFilter.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+
+namespace DZtheVlumeOfTheSelectedWalls
+{
+    internal class WallFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Wall;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
